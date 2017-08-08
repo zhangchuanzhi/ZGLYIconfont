83394280b936f5c6b6bc85da9995dd1f402a5904
zhangzhi0903 <18310127735@163.com>
2017-08-08 10:10:59 +0800

    在iOS开发中使用iconfont图标

--- ZGLYIconfont/ViewController.m
+++ ZGLYIconfont/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "UIImage+TBCityIconFont.h"
 @interface ViewController ()
 
 @end
@@ -16,13 +16,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    //    imageView
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(100, 50, 30, 30)];
+    [self.view addSubview:imageView];
+    imageView.image = [UIImage iconWithInfo:TBCityIconInfoMake(@"\U0000e603", 30, [UIColor redColor])];
+    //    button
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    button.frame = CGRectMake(100, 100, 40, 40);
+    [self.view addSubview:button];
+    [button setImage:[UIImage iconWithInfo:TBCityIconInfoMake(@"\U0000e60c",50, [UIColor redColor])] forState:UIControlStateNormal];
+    [button setTintColor:[UIColor greenColor]];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 
